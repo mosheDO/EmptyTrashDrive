36ba3249544734ffd78aa12548d48be5bd00e28a
moshe <moshedoppelt@gmail.com>
2020-07-27 10:27:05 +0300

    Create EmptyTrash.sh

---
+++ EmptyTrash.sh
@@ -0,0 +1,5 @@
+curl -X DELETE \
+  https://www.googleapis.com/drive/v3/files/trash \
+  -H 'Authorization: Bearer [PUT_THE_TOKEN_HERE]' \
+  -H 'Host: www.googleapis.com' \
+  -H 'cache-control: no-cache'
